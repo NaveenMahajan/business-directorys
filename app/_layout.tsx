2f5f019d38c0f2d60aa9f49ccdbd41c8407890bb
NaveenMahajan <naveenmahajan12345678@gmail.com>
2026-02-14 13:42:15 +0530

    added tokencache

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,4 @@
+import { tokenCache } from "@/business-directory/utils/tokenCache";
 import { ClerkProvider } from "@clerk/clerk-expo";
 import Constants from "expo-constants";
 import { Stack } from "expo-router";
@@ -6,6 +7,7 @@ export default function RootLayout() {
   return (
     <ClerkProvider
       publishableKey={Constants.expoConfig?.extra?.clerkPublishableKey}
+      tokenCache={tokenCache}
     >
       <Stack
         screenOptions={{
